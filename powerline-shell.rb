8d7fc136674f42fb05481083c4e3e7a414bf6c02
paulhybryant <paulhybryant@gmail.com>
2015-01-19 20:34:48 -0800

    .

--- powerline-shell.rb
+++ powerline-shell.rb
@@ -5,6 +5,10 @@ class PowerlineShell < Formula
 
   def install
     system "cp -r * #{prefix}/"
+    # ruby uses /bin/sh for system command. For system where /bin/sh does not
+    # support pushd / popd, the following line will fail.
+    # For example, in ubuntu, /bin/sh points to /bin/dash. If /bin/sh is not a
+    # symlink, /bin/sh does not support pushd / popd either.
     system "pushd #{prefix}; echo $PWD; ./install.py; popd"
     system "ln -f -s #{prefix}/powerline-shell.py #{HOMEBREW_PREFIX}/bin"
   end
