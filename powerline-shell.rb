0c7c5d5136cc678ff7d6d8e7685f0da5ad761053
paulhybryant <paulhybryant@gmail.com>
2015-02-05 15:40:50 -0800

    Update powerline-shell to install in correct dir.

--- powerline-shell.rb
+++ powerline-shell.rb
@@ -4,16 +4,13 @@ class PowerlineShell < Formula
   version "head"
 
   def install
+    system "./install.py"
     system "cp -r * #{prefix}/"
-    # ruby uses /bin/sh for system command. For system where /bin/sh does not
-    # support pushd / popd, the following line will fail.
-    # For example, in ubuntu, /bin/sh points to /bin/dash. If /bin/sh is not a
-    # symlink, /bin/sh does not support pushd / popd either.
-    system "pushd #{prefix}; echo $PWD; ./install.py; popd"
-    system "cd #{HOMEBREW_PREFIX}/bin; ln -f -s ../Cellar/#{name}/#{version}/powerline-shell.py ./"
+    system "mkdir -p #{bin}"
+    system "ln -f -s #{prefix}/powerline-shell.py #{bin}/powerline-shell.py"
   end
 
   test do
-    system "true"
+    system "#{bin}/powerline-shell.py -h"
   end
 end
