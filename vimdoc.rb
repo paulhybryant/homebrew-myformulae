4b7e9493d0d29f653d607ce9baff5d33c4876c0b
paulhybryant <paulhybryant@gmail.com>
2015-02-05 15:16:47 -0800

    Add vimdoc formula

---
+++ vimdoc.rb
@@ -0,0 +1,33 @@
+class Vimdoc < Formula
+  homepage ""
+  url "https://github.com/google/vimdoc.git"
+  sha1 ""
+  version "HEAD"
+
+  depends_on :python3
+
+  def install
+    system "python3", "setup.py", "config"
+    system "python3", "setup.py", "build"
+    system "python3", "setup.py", "install", "--install-scripts=#{HOMEBREW_PREFIX}/bin", "--record=#{HOMEBREW_PREFIX}/log/vimdoc.log"
+  end
+
+  # def uninstall
+    # system "cat #{HOMEBREW_PREFIX}/log/vimdoc.log"
+    # # system "cat #{HOMEBREW_PREFIX}/log/vimdoc.log | xargs rm -r"
+    # # system "rm -r #{HOMEBREW_PREFIX}/bin/vimdoc"
+  # end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test vimdoc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "vimdoc", "-h"
+  end
+end
