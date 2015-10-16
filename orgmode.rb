ab1d0f858e65dfb4c51cfda0f0e0f2646aa23154
paulhybryant <paulhybryant@gmail.com>
2015-10-16 15:05:59 +0800

    .

---
+++ orgmode.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class OrgMode < Formula
+  homepage 'http://orgmode.org/'
+  url 'http://orgmode.org/org-8.3.2.tar.gz'
+  sha1 'fcb1c74c84387979a883431012930a0a63fc5138'
+  version '8.3.2'
+
+  head 'git://orgmode.org/org-mode.git'
+
+  def install
+    system "make", "all"
+    # (share+"emacs/site-lisp").install "lisp" => "org"
+    # (share+"emacs/site-lisp/org").install "contrib/lisp" => "contrib"
+    info.install 'doc/org' => 'org.info'
+  end
+end
