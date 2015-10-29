093c025b69b9a45bb1ba4b00d381992c095aae30
paulhybryant <paulhybryant@gmail.com>
2015-10-29 12:21:41 +0800

    .

---
+++ trash-cli.rb
@@ -0,0 +1,12 @@
+class TrashCli < Formula
+  homepage "https://github.com/andreafrancia/trash-cli"
+  head "://github.com/andreafrancia/trash-cli.git"
+
+  def install
+    system "python", "setup.py", "install", "--install-scripts=#{bin}", "--record=#{prefix}/install.log"
+  end
+
+  test do
+    system "#{bin}/trash", "-v"
+  end
+end
