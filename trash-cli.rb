d2f34e8df49aad19ad1c090c8581d578f00e8b97
paulhybryant <paulhybryant@gmail.com>
2015-10-29 12:37:03 +0800

    .

--- trash-cli.rb
+++ trash-cli.rb
@@ -2,9 +2,11 @@ class TrashCli < Formula
   homepage "https://github.com/andreafrancia/trash-cli"
   head "https://github.com/andreafrancia/trash-cli.git"
 
+  depends_on "trash" if OS.mac?
+
   def install
     system "python", "setup.py", "install", "--install-scripts=#{bin}", "--record=#{prefix}/install.log"
-  end
+  end if OS.linux?
 
   test do
     system "#{bin}/trash", "-v"
