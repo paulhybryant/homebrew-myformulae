492755e1dbf85e2d87837ae1cf9d25c0b7ecffb9
paulhybryant <paulhybryant@gmail.com>
2015-11-13 12:03:58 +0800

    .

--- xbindkeys.rb
+++ xbindkeys.rb
@@ -1,7 +1,8 @@
-class TrashCli < Formula
+class Xbindkeys < Formula
   homepage "http://www.nongnu.org/xbindkeys/"
   head "http://git.savannah.gnu.org/cgit/xbindkeys.git"
   url "http://www.nongnu.org/xbindkeys/xbindkeys-1.8.6.tar.gz"
+  sha256 "6c0d18be19fc19ab9b4595edf3a23c0a6946c8a5eb5c1bc395471c8f9a710d18"
 
   def install
     system "./configure", "--prefix=#{prefix}"
