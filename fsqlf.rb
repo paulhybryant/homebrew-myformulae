d93631ee01342ba7829f87d4ff27e3678f93efae
paulhybryant <paulhybryant@gmail.com>
2015-12-07 21:19:14 +0800

    .

--- fsqlf.rb
+++ fsqlf.rb
@@ -2,8 +2,6 @@ class Fsqlf < Formula
   homepage "https://github.com/dnsmkl/fsqlf"
   head "https://github.com/dnsmkl/fsqlf.git"
 
-  patch :DATA
-
   def install
     system "make", "fsqlf"
     # prefix.install "fsqlf"
@@ -14,17 +12,3 @@ class Fsqlf < Formula
     system "#{bin}/fsqlf", "--help"
   end
 end
-
-__END__
-diff --git a/makefile b/makefile
-index 51a0462..e17403c 100644
---- a/makefile
-+++ b/makefile
-@@ -23,7 +23,6 @@ PREFIX=/usr/local
- EXEC_CLI=fsqlf
- EXEC_GUI=wx_fsqlf
- CC=gcc
--CFLAGS+=-m32
- CXX=g++
- CXXFLAGS+= `wx-config --cxxflags`
- LDFLAGS+= `wx-config --libs`
