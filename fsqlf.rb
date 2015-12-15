1164291aecd6a9fb2b5ad2118f48c9a2c79d9933
paulhybryant <paulhybryant@gmail.com>
2015-12-15 16:21:37 +0800

    .

--- fsqlf.rb
+++ fsqlf.rb
@@ -1,6 +1,7 @@
 class Fsqlf < Formula
   homepage "https://github.com/dnsmkl/fsqlf"
   head "https://github.com/dnsmkl/fsqlf.git"
+  patch :Data
 
   def install
     system "make", "fsqlf"
@@ -12,3 +13,33 @@ class Fsqlf < Formula
     system "#{bin}/fsqlf", "--help"
   end
 end
+__END__
+diff --git a/makefile b/makefile
+index b85eae2..c95a8f0 100644
+--- a/makefile
++++ b/makefile
+@@ -4,6 +4,7 @@ CFLAGS+=-std=c99
+ CFLAGS+=-Wall
+ CFLAGS+=-pedantic-errors
+ CFLAGS+=-g
++CFLAGS+=-fPIC
+
+ CXXFLAGS+=-DVERSION=\"$(VERSION)\"
+
+@@ -26,14 +27,10 @@ else
+ 	CXX=g++
+ 	CXXFLAGS+= `wx-config --cxxflags`
+ 	LDFLAGS+= `wx-config --libs`
+-	ifneq (Darwin, ${_system_type})
+-		CFLAGS+=-m32
+-	else
+-		CFLAGS+=-m64
+-	endif
++	CFLAGS+=-m64
+ endif
+
+-ifeq (Darwin, ${_system_type})
++ifeq (Darwin, `uname`)
+ 	LIBNAME=libfsqlf.dylib
+ 	LIBFLAGS=-dynamiclib
+ else
