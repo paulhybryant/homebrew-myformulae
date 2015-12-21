4fc558e986bc57068603021c0f5cbfd513f283dd
paulhybryant <paulhybryant@gmail.com>
2015-12-21 20:40:23 +0800

    .

--- xbindkeys.rb
+++ xbindkeys.rb
@@ -4,6 +4,8 @@ class Xbindkeys < Formula
   url "http://www.nongnu.org/xbindkeys/xbindkeys-1.8.6.tar.gz"
   sha256 "6c0d18be19fc19ab9b4595edf3a23c0a6946c8a5eb5c1bc395471c8f9a710d18"
 
+  depends_on "guile"
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
