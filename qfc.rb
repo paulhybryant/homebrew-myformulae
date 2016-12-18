475dbfeb5e912090d8963bf13b4dea097f3ba12a
paulhybryant <paulhybryant@gmail.com>
2016-12-18 21:37:12 +0800

    .

---
+++ qfc.rb
@@ -0,0 +1,14 @@
+class Qfc < Formula
+  homepage "https://github.com/pindexis/qfc.git"
+  head "https://github.com/pindexis/qfc.git"
+
+  def install
+    prefix.install Dir["bin"]
+    prefix.install Dir["qfc"]
+    ln_sf "#{prefix}/qfc", "#{HOMEBREW_PREFIX}/lib/python2.7/site-packages"
+  end
+
+  test do
+    system "#{bin}/qfc -h"
+  end
+end
