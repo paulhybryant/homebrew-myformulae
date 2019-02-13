9023669df94e5f57d097d0f493cb2043719b61d7
paulhybryant <paulhybryant@gmail.com>
2019-02-13 10:23:59 +0800

    .

--- aria2-daemon.rb
+++ aria2-daemon.rb
@@ -11,27 +11,28 @@ class Aria2Daemon < Formula
     share.install "aria2.session"
   end
 
-  plist_options :manual => "aria2-daemon"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{HOMEBREW_PREFIX}/bin/aria2c</string>
-        <string>--conf-path</string>
-        <string>#{share}/aria2.conf</string>
-        <string>--input-file</string>
-        <string>#{share}/aria2.session</string>
-      </array>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
-    EOS
+  if OS.mac?
+    plist_options :manual => "aria2-daemon"
+    def plist; <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{HOMEBREW_PREFIX}/bin/aria2c</string>
+          <string>--conf-path</string>
+          <string>#{share}/aria2.conf</string>
+          <string>--input-file</string>
+          <string>#{share}/aria2.session</string>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
+      EOS
+    end
   end
 end
