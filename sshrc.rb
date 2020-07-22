c724ce35033d57562f058d2388ae3524ae1768d6
paulhybryant <paulhybryant@gmail.com>
2020-07-22 11:10:23 +0800

    .

---
+++ sshrc.rb
@@ -0,0 +1,23 @@
+class Sshrc < Formula
+  desc "Bring your .bashrc, .vimrc, etc. with you when you SSH"
+  homepage "https://github.com/Russell91/sshrc"
+  url "https://raw.githubusercontent.com/paulhybryant/dotfiles/master/misc/sshrc-0.6.2.tar.gz"
+  sha256 "f3cc4a75b7905110d50db3deb11415851753a796e2c451c5dea2ee5a7f07fd33"
+
+  bottle :unneeded
+
+  def install
+    bin.install %w[sshrc moshrc]
+  end
+
+  test do
+    touch testpath/".sshrc"
+    (testpath/"ssh").write <<~EOS
+      #!/bin/sh
+      true
+    EOS
+    chmod 0755, testpath/"ssh"
+    ENV.prepend_path "PATH", testpath
+    system "#{bin}/sshrc", "localhost"
+  end
+end
